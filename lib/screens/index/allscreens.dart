27bacdcb6b5af661a3beca8e5c34b251f348fca5
PascalM13 <passi.musenbrock@gmail.com>
2022-06-08 11:58:58 +0200

    Init empty project skeleton

--- lib/screens/index/allscreens.dart
+++
@@ -1,5 +0,0 @@
-export '../activity/activity.dart';
-export '../activity/addactivity.dart';
-export '../dashboard/dashboard.dart';
-export '../sensor/sensor.dart';
-export '../auth/signup.dart';
---
+++ lib/theme/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFFF5F7FB);
+const Color secondery = Color(0xFF565FE2);
+const Color accentColor = Color(0xFF74C8E4);
